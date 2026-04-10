2511205cfe9bd97ec5e9db0ecabb70b103613d5c
Abhishek Gupta <abhishekgupta.lu@gmail.com>
2026-04-10 12:20:45 +0530

    added more questions...

--- collection/src/Map/Main.java
+++ collection/src/Map/Main.java
@@ -631,15 +631,17 @@ public class Main {
 //Note:
 //    If there are multiple answers, please return the first one.
 //    If there is no element found, return -1.
-    public static int firstElementKTime(int[] arr, int k) {
-        // write code
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            map.put(arr[i],map.getOrDefault(arr[i],0) + 1);
-            if(map.get(arr[i])==k){
-                return arr[i];
-            }
-        }
-        return -1;
-    }
+//    public static int firstElementKTime(int[] arr, int k) {
+//        // write code
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for(int i=0;i<arr.length;i++){
+//            map.put(arr[i],map.getOrDefault(arr[i],0) + 1);
+//            if(map.get(arr[i])==k){
+//                return arr[i];
+//            }
+//        }
+//        return -1;
+//    }
+
+
 }
