ef9c3e5d09ef5dcdae15d5ec3e6b884555d03449
Abhishek Gupta <abhishekgupta.lu@gmail.com>
2026-05-04 23:44:29 +0530

    added more questions...

--- collection/src/Map/Main.java
+++ collection/src/Map/Main.java
@@ -1224,6 +1224,26 @@ public class Main {
 //        return count;
 //    }
 
-//
+//Given a number n, determine whether it is a prime number or not.
+//Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+//    public static boolean isPrime(int n) {
+//        // code here
+//        int count =0;
+//        if(n<=1){
+//            return false;
+//        }else{
+//            for(int i=1; i<=n; i++){
+//                if(n%i==0){
+//                    count++;
+//                }
+//            }
+//        }
+//        if(count>2){
+//            return false;
+//        }else{
+//            return true;
+//        }
+//    }
+
 
 }
