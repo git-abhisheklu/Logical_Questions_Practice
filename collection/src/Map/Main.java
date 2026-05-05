f47d66852b98a0adba40bba240dc474cdaa30f1a
Abhishek Gupta <abhishekgupta.lu@gmail.com>
2026-05-05 12:14:48 +0530

    added more questions...

--- collection/src/Map/Main.java
+++ collection/src/Map/Main.java
@@ -1245,5 +1245,24 @@ public class Main {
 //        }
 //    }
 
+//You are given a number  and you have to print your answer according to the following:
+//If the number is divisible by 3, you print "Fizz" (without quotes)
+//If the number is divisible by 5, you print "Buzz" (without quotes)
+//If the number is divisible by both 3 and 5, you print "FizzBuzz" (without quotes)
+//In any other case, you print the number itself
+//Note: You should add a newline character after print() statement.
+    public static void fizzBuzz(int number) {
+        // code here.
+        if(number%3==0 && number%5==0){
+            System.out.println("FizzBuzz");
+        } else if(number%3==0){
+            System.out.println("Fizz");
+        } else if(number%5==0){
+            System.out.println("Buzz");
+        }else{
+            System.out.println(number);
+        }
+    }
+
 
 }
